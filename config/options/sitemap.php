69ee5b15a6f9c6ac8b301cf1ea8b0dc5e3b8c6ee
Tobias Möritz <tobias@moeritz.cc>
2024-01-18 17:55:47 +0100

    fallback for pages that don't have a correct modified date
    Signed-off-by: Tobias Möritz <tobias@moeritz.cc>

--- config/options/sitemap.php
+++ config/options/sitemap.php
@@ -1,37 +1,37 @@
 <?php
 
-use tobimori\Seo\Sitemap\SitemapIndex;
 use Kirby\Toolkit\Obj;
+use tobimori\Seo\Sitemap\SitemapIndex;
 
 return function (SitemapIndex $sitemap) {
-  $exclude = option('tobimori.seo.sitemap.excludeTemplates', []);
-  $pages = site()->index()->filter(fn ($page) => $page->metadata()->robotsIndex()->toBool() && !in_array($page->intendedTemplate()->name(), $exclude));
+	$exclude = option('tobimori.seo.sitemap.excludeTemplates', []);
+	$pages = site()->index()->filter(fn ($page) => $page->metadata()->robotsIndex()->toBool() && !in_array($page->intendedTemplate()->name(), $exclude));
 
-  if ($group = option('tobimori.seo.sitemap.groupByTemplate')) {
-    $pages = $pages->group('intendedTemplate');
-  }
+	if ($group = option('tobimori.seo.sitemap.groupByTemplate')) {
+		$pages = $pages->group('intendedTemplate');
+	}
 
-  if (is_a($pages->first(), 'Kirby\Cms\Page')) {
-    $pages = $pages->group(fn () => 'pages');
-  }
+	if (is_a($pages->first(), 'Kirby\Cms\Page')) {
+		$pages = $pages->group(fn () => 'pages');
+	}
 
-  foreach ($pages as $group) {
-    $index = $sitemap->create($group ? $group->first()->intendedTemplate()->name() : 'pages');
+	foreach ($pages as $group) {
+		$index = $sitemap->create($group ? $group->first()->intendedTemplate()->name() : 'pages');
 
-    foreach ($group as $page) {
-      $url = $index->createUrl($page->metadata()->canonicalUrl())
-        ->lastmod($page->modified())
-        ->changefreq(is_callable($changefreq = option('tobimori.seo.sitemap.changefreq')) ? $changefreq($page) : $changefreq)
-        ->priority(is_callable($priority = option('tobimori.seo.sitemap.priority')) ? $priority($page) : $priority);
+		foreach ($group as $page) {
+			$url = $index->createUrl($page->metadata()->canonicalUrl())
+				->lastmod($page->modified() ?? intval(date('c')))
+				->changefreq(is_callable($changefreq = option('tobimori.seo.sitemap.changefreq')) ? $changefreq($page) : $changefreq)
+				->priority(is_callable($priority = option('tobimori.seo.sitemap.priority')) ? $priority($page) : $priority);
 
-      if (kirby()->languages()->count() > 1 && kirby()->language() !== null) {
-        $url->alternates(
-          kirby()->languages()->map(fn ($language) => new Obj([
-            'hreflang' => $language->code() !== kirby()->language()->code() ? 'x-default' : $language->code(),
-            'href' => $page->url($language->code()),
-          ]))->toArray()
-        );
-      }
-    }
-  };
+			if (kirby()->languages()->count() > 1 && kirby()->language() !== null) {
+				$url->alternates(
+					kirby()->languages()->map(fn ($language) => new Obj([
+						'hreflang' => $language->code() !== kirby()->language()->code() ? 'x-default' : $language->code(),
+						'href' => $page->url($language->code()),
+					]))->toArray()
+				);
+			}
+		}
+	};
 };
