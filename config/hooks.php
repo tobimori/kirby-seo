c01af517c05e5305b2d43d7910f015d35498d16c
tobimori <tobias@moeritz.io>
2024-01-26 02:03:11 +0100

    bump versionn umber

--- config/hooks.php
+++ config/hooks.php
@@ -5,7 +5,12 @@ use Kirby\Cms\Page;
 return [
 	'page.render:before' => function (string $contentType, array $data, Page $page) {
 		if (option('tobimori.seo.generateSchema')) {
-			$page->schema('WebSite')->url($page->metadata()->canonicalUrl())->copyrightYear(date('Y'))->description($page->metadata()->metaDescription())->name($page->metadata()->metaTitle())->headline($page->metadata()->title());
+			$page->schema('WebSite')
+				->url($page->metadata()->canonicalUrl())
+				->copyrightYear(date('Y'))
+				->description($page->metadata()->metaDescription())
+				->name($page->metadata()->metaTitle())
+				->headline($page->metadata()->title());
 		}
-	}
+	},
 ];
