49f9851f6f66c4bf7c83a79b0c46852367820a8b
Ove Numrich <47149622+ovenum@users.noreply.github.com>
2025-08-21 13:46:52 +0200

    fix: Storage for the page is immutable when saving page with kirby-seo fields
    Trying the fix the `Storage for the page is immutable` error message that’s popping when using the plugin with Kirby 5.x.
    
    This basically only implements what Nico suggested in the Discord chat. 
    
    Currently i can’t reproduce the issue reliably so i don’t know if this will fix it. Maybe someone can give it a try or let me know how to reproduce?
    
    Signed-off-by: Ove Numrich <47149622+ovenum@users.noreply.github.com>

--- config/hooks.php
+++ config/hooks.php
@@ -1,18 +1,28 @@
 <?php
 
 use Kirby\Cms\Page;
+use Kirby\Toolkit\A;
 use Kirby\Toolkit\Str;
 
 return [
 	'page.update:after' => function (Page $newPage, Page $oldPage) {
-		foreach ($newPage->kirby()->option('tobimori.seo.robots.types') as $robots) {
-			$upper = Str::ucfirst($robots);
-			if ($newPage->content()->get("robots{$upper}")->value() === "") {
-				$newPage = $newPage->update([
-					"robots{$upper}" => 'default'
-				]);
-			}
-		}
+		$updates = A::reduce(
+			$newPage->kirby()->option('tobimori.seo.robots.types'),
+			function ($carry, $robots) use ($newPage) {
+				$upper = Str::ucfirst($robots);
+
+				if ($newPage->content()->get("robots{$upper}")->value() === '') {
+					$carry["robots{$upper}"] = 'default';
+				}
+
+				return $carry;
+			},
+			[]
+		);
+
+		$newPage = $newPage->update($updates);
+
+		return $newPage;
 	},
 	'page.render:before' => function (string $contentType, array $data, Page $page) {
 		if (!class_exists('Spatie\SchemaOrg\Schema')) {
