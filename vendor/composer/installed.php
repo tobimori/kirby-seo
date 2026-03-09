9f449bc10b8aefaa715258d44ad0e0862a4f5886
tobimori <tobias@moeritz.io>
2026-03-09 23:20:36 +0100

    require ext-imagick for svg alt text gen

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,8 +3,8 @@
 return array(
 	'root' => array(
 		'name' => 'tobimori/kirby-seo',
-		'pretty_version' => '2.0.0-alpha.14',
-		'version' => '2.0.0.0-alpha14',
+		'pretty_version' => '2.0.0-beta.1',
+		'version' => '2.0.0.0-beta1',
 		'reference' => null,
 		'type' => 'kirby-plugin',
 		'install_path' => __DIR__ . '/../../',
@@ -22,8 +22,8 @@ return array(
 			'dev_requirement' => false,
 		),
 		'tobimori/kirby-seo' => array(
-			'pretty_version' => '2.0.0-alpha.14',
-			'version' => '2.0.0.0-alpha14',
+			'pretty_version' => '2.0.0-beta.1',
+			'version' => '2.0.0.0-beta1',
 			'reference' => null,
 			'type' => 'kirby-plugin',
 			'install_path' => __DIR__ . '/../../',
